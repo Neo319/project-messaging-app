4e2d1fa2277d5dd7b1289619f6014d599352f5d1
CastorDisaster <alexmnevins@gmail.com>
2024-11-19 10:47:52 -0500

    create forms for logins and signups

--- Client_App/src/routes/login.jsx
+++ Client_App/src/routes/login.jsx
@@ -1,5 +1,21 @@
 import "../App.css";
 
 export default function Login() {
-  return <h1>Hi. Login page here.</h1>;
+  return (
+    <>
+      <h1>Hi. Login Page here.</h1>
+
+      <form action="">
+        <label htmlFor="username">Username: </label>
+        <input required="true" type="text" name="username" />
+        <br />
+
+        <label htmlFor="password">Enter Password: </label>
+        <input required="true" type="password2" name="password" />
+        <br />
+
+        <input type="submit" value="Login" />
+      </form>
+    </>
+  );
 }
--- Client_App/src/routes/signup.jsx
+++ Client_App/src/routes/signup.jsx
@@ -1,14 +1,28 @@
 import "../App.css";
+import icon from "../assets/messageIcon2.jpg";
 
 export default function SignUp() {
   return (
     <>
-      <img
-        src="https://img.freepik.com/free-vector/message-vector-logo-design_557339-558.jpg?t=st=1732028489~exp=1732032089~hmac=2f3f941a9f2ef564dc9b1c7732649b815698820a657eda6169fdc136e68bfa96&w=1380"
-        alt="Messager Icon"
-        className="icon message"
-      />
+      <img src={icon} alt="Messager Icon" className="icon message" />
       <h1>Hi. Signup page here.</h1>
+
+      <h2>Create New User here.</h2>
+      <form action="">
+        <label htmlFor="username">Username: </label>
+        <input required="true" type="text" name="username" />
+        <br />
+
+        <label htmlFor="password">Enter Password: </label>
+        <input required="true" type="password2" name="password" />
+        <br />
+
+        <label htmlFor="password2">Confirm Password: </label>
+        <input required="true" type="password" name="password2" />
+        <br />
+
+        <input type="submit" value="Create User" />
+      </form>
     </>
   );
 }
