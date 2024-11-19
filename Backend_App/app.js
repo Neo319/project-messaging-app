7ae39ae4fffdd4299a3a52c1efbe37473ef29183
CastorDisaster <alexmnevins@gmail.com>
2024-11-19 11:01:50 -0500

    create base for backend server

--- Backend_App/app.js
+++ Backend_App/app.js
@@ -1,3 +1,15 @@
 require("dotenv").config;
 
 const express = require("express");
+const port = process.env.PORT || 2000;
+
+//starting app
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("Welcome to project Message board (WIP)");
+});
+
+app.listen(port, () => {
+  console.log("app listening on port: ", port);
+});
