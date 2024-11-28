96092aaca0be1b47c66331869f15e224dd9fb721
CastorDisaster <alexmnevins@gmail.com>
2024-11-28 11:07:36 -0500

    clean up landing page; create header component

--- docs/src/App.jsx
+++ docs/src/App.jsx
@@ -1,60 +1,29 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import footer from "./components/footer";
-
-import icon from "./assets/messageIcon2.jpg";
+import header from "./components/header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
-      <p>Image?</p>
-      <img src={icon} alt="messager logo" className="icon message" />
+      {header()}
+      <p>description...</p>
 
       {/* ROUTES */}
       <p>Client-side routing test go!! </p>
       <ul>
-        {/* SIGNUP */}
         <li>
           <a href="signup" target="_blank" rel="noopener noreferrer">
             Sign up page
           </a>
         </li>
-        {/* LOGIN */}
         <li>
           <a href="login" target="_blank" rel="noopener noreferrer">
             Login page
           </a>
         </li>
-        {/* DASHBOARD */}
         <li>
           <a href="dashboard">Dashboard</a>
         </li>
-        {/* MESSAGES */}
         <li>
           <a href="message" rel="noopener noreferrer">
             Message
---
+++ docs/src/components/header.jsx
@@ -0,0 +1,15 @@
+import "../App.css";
+import icon from "../assets/messageIcon2.jpg";
+import reactLogo from "../assets/react.svg";
+export default function header() {
+  return (
+    <>
+      <header>
+        <img src={icon} alt="messager logo" className="headerIcon" />
+        <img src={reactLogo} alt="react logo" className="reactIcon" />
+
+        <h1>Project Message App</h1>
+      </header>
+    </>
+  );
+}
