61bf8cbc72c6b0689afabc214d57c00728e9f9f6
CastorDisaster <alexmnevins@gmail.com>
2024-11-29 15:33:28 -0500

    render contacts data on dashboard

--- docs/src/routes/dashboard.jsx
+++ docs/src/routes/dashboard.jsx
@@ -3,11 +3,42 @@ import "../styles/App.css";
 import header from "../components/header";
 import UserPanel from "../components/userPanel";
 
+import { useState, useEffect } from "react";
+
 export default function Dashboard() {
+  const [contacts, setContacts] = useState();
+
+  // let token = localStorage.getItem(token);
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/app/messages`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setContacts(data);
+        return data;
+      });
+  }, []);
+
+  if (!localStorage.getItem("token")) {
+    return <>Dashboard (err:not logged in!)</>;
+  }
+
   return (
     <>
       {header()}
-      <h1>Hi. Dashboard page here.</h1>
+
+      <div className="panel" id="contactsList">
+        Contacts list 👀
+        <p>contacts: {JSON.stringify(contacts)}</p>
+      </div>
+
+      <div id="profile">Customize profile ⚙️</div>
+
       {UserPanel()}
     </>
   );
